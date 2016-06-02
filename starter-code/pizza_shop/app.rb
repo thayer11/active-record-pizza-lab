50dc50613f9abfb16ad38ce3eb48e70fc4ad8295
Ben Hulan <benhulan@gmail.com>
2016-06-02 16:01:01 -0700

    adds in class solution part 1

--- starter-code/pizza_shop/app.rb
+++ starter-code/pizza_shop/app.rb
@@ -1,2 +1,23 @@
 class PizzaShop < Sinatra::Base
+
+    # index lists all pizzas
+    get "/pizzas" do 
+        @pizzas = Pizza.all
+        # "We have pizzas"
+        erb :pizzas
+    end
+
+    # new = render a new pizza form
+    get "/pizzas/new" do
+        erb :new
+    end
+
+    # create new pizza
+    post "/pizzas" do
+        params = {name: "Mrs Robinson", sauce: "red", cheese: true, mushrooms: true, extra_toppings: "virgin olive oil"}
+        @pizza = Pizza.new(params);
+        @pizza.save
+        "New pizza saved"
+    end
+
 end
